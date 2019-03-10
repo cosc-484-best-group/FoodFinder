27489d0720f581c2c9d0a70b9812f48b4bf9774f
mstill3 <stillwell006@gmail.com>
2019-03-10 21:11:43 +0000

    Changed port 3000 to 80 and added sudo to launch.sh

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const port = 3000;
+const port = 80;
 
 
 var MongoClient = require('mongodb').MongoClient;
--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-node app
+sudo node app
