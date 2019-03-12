5c78ea0c4e94638a441b2ae66580f5d81c638ab5
mstill3 <stillwell006@gmail.com>
2019-03-11 20:58:02 -0400

    Testing logic

--- js/mine/full.js
+++ js/mine/full.js
@@ -3,9 +3,22 @@ fullheight();
 function fullheight() 
 {
     var element = document.getElementById('main');
-    var windowHeight = document.body.scrollHeight + "px";
-    alert(windowHeight);
+    var h = document.body.scrollHeight;
+    var windowHeight = h + "px";
+    alert(h);
+    var he = getActualHeight();
+    alert(he);
     element.style.height = windowHeight;
+}
+
+function getActualHeight() 
+{
+    var actualHeight = window.innerHeight ||
+                      document.documentElement.clientHeight ||
+                      document.body.clientHeight ||
+                      document.body.offsetHeight;
+
+    return actualHeight;
 }
 
 window.onresize = function(event)
