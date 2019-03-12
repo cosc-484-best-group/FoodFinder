bb248b64a30f841859cc4723c349922bf11d965a
mstill3 <stillwell006@gmail.com>
2019-03-11 20:43:46 -0400

    Resize fix

--- js/mine/full.js
+++ js/mine/full.js
@@ -2,12 +2,10 @@
 fullheight();
 function fullheight() 
 {
-    var elements = document.getElementById('main');
+    var element = document.getElementById('main');
     var windowheight = window.innerHeight + "px";
     alert(windowheight);
-    for(let el in elements)
-        if(elements.hasOwnProperty(el))
-            elements[el].style.height = windowheight;
+    element.style.height = windowheight;
 }
 
 window.onresize = function(event)
