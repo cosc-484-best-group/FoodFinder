fa3459624d46512a58b87c3ae39b55e3f2b5d2cf
Max Coplan <mchcopl@gmail.com>
2019-04-04 16:06:44 -0400

    fixed logging

--- server/restartServer.sh
+++ server/restartServer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 screen -X -S node quit;
 screen -d -m -S node 
-screen -X -S node stuff 'node ~/Github/FoodFinder app.js 2>&1 | tee -a ~/logs/$(date +"%F+%H:%M:%S+%N").log';
+screen -X -S node stuff 'node ~/Github/FoodFinder app.js 2>&1 | tee -a ~/logs/$(date +"%F+%H:%M:%S+%N").log\n';
