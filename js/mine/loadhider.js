929b852f89dd1da0070eab08c4ee5bec82cbaaad
mstill3 <stillwell006@gmail.com>
2019-04-19 01:33:19 -0400

    changed loading colors and added fade effect

--- js/mine/loadhider.js
+++ js/mine/loadhider.js
@@ -1,5 +1,16 @@
-function removeload()
+async function removeload()
 {
     var load_screen = document.getElementById("load_screen");
+
+    for(var opacity = 1.0; opacity > 0.0; opacity-=0.05)
+    {
+        load_screen.style.opacity = opacity; 
+        await sleep(1);
+        console.log(opacity);
+    }
     document.body.removeChild(load_screen);
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
