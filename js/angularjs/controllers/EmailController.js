fffbc077865f69e616f5ac81909769ccc50d461a
mstill3 <stillwell006@gmail.com>
2019-05-02 00:55:25 -0400

    email contact

--- js/angularjs/controllers/EmailController.js
+++ js/angularjs/controllers/EmailController.js
@@ -4,8 +4,30 @@ app.controller('EmailController', ['$scope', '$http', function ($scope, $http)
 
     $scope.sendMessage = function(subject, message) 
     {
-        console.log("Subject: " + subject);
-        console.log("Message: " + message);
+
+        // Sends email off
+        var nodemailer = require('nodemailer');
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,  //any
+            secure: true,
+            auth: {
+                user: 'bobertb492@gmail.com',
+                pass: 'Bobby15Cool!'
+            }
+        });
+
+        let mailOptions = {
+            from: '"Food Finder" <bobertb492@gmail.com>',
+            to: 'stillwell006@gmail.com',
+            subject: subject,
+            html: message
+        };
+
+        transporter.sendMail(mailOptions);
+        alert('Email Sent!');
+    
     }
 
 }]);
