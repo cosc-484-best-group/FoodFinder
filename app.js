37b8b78b485efe9f19c278c4be18fa024299bce1
cgood404 <chrisgood404@gmail.com>
2019-05-07 18:25:01 -0400

    fixing login issues

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+  extended: false
 })); 
   
 
