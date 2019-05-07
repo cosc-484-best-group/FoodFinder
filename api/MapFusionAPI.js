11e8e3693574c8293548a0e27e25f6b31ae90b84
mstill3 <stillwell006@gmail.com>
2019-05-06 22:08:52 -0400

    fix ???

--- api/MapFusionAPI.js
+++ api/MapFusionAPI.js
@@ -282,32 +282,8 @@ function yelpfusion(send=true, args, res)
 {
   return new Promise(function(resolve, reject) 
   {
-
-    var o = {};
-    o.o = o;
+    console.log("dsdsd: " + JSON.stringify(args));
     
-    // Note: cache should not be re-used by repeated calls to JSON.stringify.
-    var cache = [];
-    JSON.stringify(args, function(key, value) {
-        if (typeof value === 'object' && value !== null) {
-            if (cache.indexOf(value) !== -1) {
-                // Duplicate reference found
-                try {
-                    // If this value does not reference a parent it can be deduped
-                    return JSON.parse(JSON.stringify(value));
-                } catch (error) {
-                    // discard key if value cannot be deduped
-                    return;
-                }
-            }
-            // Store value in our collection
-            cache.push(value);
-        }
-        return value;
-    });
-    cache = null; // Enable garbage collection
-
-
     var yelp_api_key = args.yelp_api_key;
     if(!yelp_api_key)
     {
